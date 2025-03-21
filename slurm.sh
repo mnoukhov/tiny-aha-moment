53b184e4d3c25b66e2fd143cfcd0589e9573014a
Michael Noukhovitch <noukhovm@mila.quebec>
2025-03-21 17:55:09 -0400

    slurm script

---
+++ slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --gres=gpu:a100l:1
+#SBATCH --mem=80G
+#SBATCH -c 4
+#SBATCH --time=8:00:00
+#SBATCH -p main
+
+module load python/3.10
+module load cudatoolkit/12.4
+export WANDB_PROJECT=r1
+export WANDB_ENTITY=mila-language-drift
+export HF_HOME=/network/scratch/n/noukhovm/huggingface
+
+uv run r1_script.py
