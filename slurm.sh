7f940cb2cdc07275531456e64dbf22fc6dec1843
Michael Noukhovitch <noukhovm@mila.quebec>
2025-03-27 14:22:11 -0400

    sbatch

--- slurm.sh
+++ slurm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --gres=gpu:a100l:1
+#SBATCH --gres=gpu:l40s:1
 #SBATCH --mem=48G
 #SBATCH -c 4
-#SBATCH --time=8:00:00
+#SBATCH --time=4:00:00
 #SBATCH -p main
 
 module load python/3.10
 module load cudatoolkit/12.4
-export WANDB_PROJECT=r1
+export WANDB_PROJECT=r1-aha-moment
 export WANDB_ENTITY=mila-language-drift
 export HF_HOME=/network/scratch/n/noukhovm/huggingface
 
-uv run r1_script.py
+uv run r1_script.py --model_name Qwen/Qwen2.5-1.5B-Instruct --liger_kernel
