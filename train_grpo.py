2b0292824393a54b6fc4bead79add8086f9748b5
Michael Noukhovitch <michaeln@allenai.org>
2025-07-26 02:06:06 +0400

    try to improve prompt

--- train_grpo.py
+++ train_grpo.py
@@ -430,8 +430,8 @@ def main(args):
         "Combine a couple numbers in a simple algebraic equation so that it equals a target number. "
         "You can use basic arithmetic operations (+, -, *, /) and each number can only be used once. "
         "Think about the reasoning process then return the final equation inside <answer> </answer> tags. "
-        "Do not include the = target inside the <answer> </answer> tags "
-        "for example after thinking step by step (5 + 2) - 3 = 4 therefore <answer>(5 + 2) - 3</answer>.\n\n"
+        "Do not include the = target inside the <answer> </answer> tags and only use each number once."
+        "For example after thinking step by step (5 + 2) - 3 = 4 therefore <answer>(5 + 2) - 3</answer>.\n\n"
         "Use the number {numbers} to create an equation that equals {target}.\n\n"
         "Let's solve this step by step."
     )
